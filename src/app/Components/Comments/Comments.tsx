7d1be565e444c0aa2882e0fe9028f443248c6acb
julianvipe <mateo-pat99@hotmail.com>
2024-03-01 16:36:09 -0500

    fix some errors on storage

--- src/app/Components/Comments/Comments.tsx
+++ src/app/Components/Comments/Comments.tsx
@@ -4,8 +4,10 @@ import styles from "./Comments.module.css";
 import CommentCard from "../CommentCard/CommentCard";
 import { ArrowLeftIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
-import { useQuery } from "react-query"
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import { useQuery } from "react-query";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 
 const IconBtn = styled.button`
   font-size: 1em;
@@ -47,12 +49,12 @@ export default function Comments(props: any) {
       start.getTime() + Math.random() * (end.getTime() - start.getTime())
     );
   }
-    const {
-      transcript,
-      listening,
-      resetTranscript,
-      browserSupportsSpeechRecognition
-    } = useSpeechRecognition();
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
 
   const [Comment, setComment] = useState({
     userName: "",
@@ -63,39 +65,43 @@ export default function Comments(props: any) {
   const [Comments, setComments] = useState([Comment]);
 
   const getComments = async () => {
-		const res = await fetch('https://jsonplaceholder.typicode.com/comments');
-		return res.json();
-	};
-  const {data, error, isLoading} = useQuery('randomFacts', getComments);
-  
-  useEffect(() => {
-    const localData=localStorage.getItem("CommentsKey");
-    if(localData){
-      JSON.parse(localData).map((localComment:any)=>{
-        const comtemp = {
-          content: localComment.content,
-          userName: localComment.userName,
-          date: localComment.date,
-        };
-        if(comtemp.content!==""){
-          addComments(comtemp);
-        }
-      })
+    if(localStorage.getItem("CommentsKey")){
+      return [];
+    }else{
+      const res = await fetch("https://jsonplaceholder.typicode.com/comments");
+    return res.json();
     }
-    else if(data){
+  };
+  const { data, error, isLoading } = useQuery("randomComments", getComments, {
+    onSuccess: (data) =>
       data.map((ApiComment: any) => {
         const comtemp = {
           content: ApiComment.body,
           userName: ApiComment.email,
           date: randomDate(new Date(2012, 0, 1), new Date()).toDateString(),
         };
-        if(comtemp.content!==""){
+        if (comtemp.content !== "") {
+          addComments(comtemp);
+        }
+      }),
+  });
+
+  useEffect(() => {
+    setComments([]);
+    const localData = localStorage.getItem("CommentsKey");
+    if (localData) {
+      JSON.parse(localData).map((localComment: any) => {
+        const comtemp = {
+          content: localComment.content,
+          userName: localComment.userName,
+          date: localComment.date,
+        };
+        if (comtemp.content !== "") {
           addComments(comtemp);
         }
       });
     }
-    
-  }, [data]);
+  }, []);
   function handleChange(event: any) {
     const { value } = event.target;
     setComment((prevComment) => {
@@ -133,9 +139,6 @@ export default function Comments(props: any) {
       });
     });
   }
-  useEffect(() => {
-    localStorage.setItem('CommentsKey', JSON.stringify(Comments));
-  });
   function hearts() {
     let hearts = [];
     for (let i = 0; i < 4; i++) {
@@ -153,26 +156,33 @@ export default function Comments(props: any) {
     }
     return <div className="flex">{hearts}</div>;
   }
-
-  function componentDidUpdate(){
+  useEffect(() => {
+    if (Comments.length > 1) {
+      localStorage.setItem("CommentsKey", JSON.stringify(Comments));
+    }
+  });
+  function componentDidUpdate() {
     console.log(transcript);
-      setComment((prevComment) => {
-        return {
-          ...prevComment,
-          content: transcript,
-          userName: "@mu?",
-          date: randomDate(new Date(2012, 0, 1), new Date()).toDateString(),
-        };
-      });
+    setComment((prevComment) => {
+      return {
+        ...prevComment,
+        content: transcript,
+        userName: "@userName",
+        date: randomDate(new Date(2012, 0, 1), new Date()).toDateString(),
+      };
+    });
   }
+  const startListening = () =>
+    SpeechRecognition.startListening({ continuous: true }).then(
+      componentDidUpdate
+    );
+  const stopListening = () =>
+    SpeechRecognition.stopListening().then(componentDidUpdate);
 
-  const startListening = () => SpeechRecognition.startListening({ continuous: true }).then(componentDidUpdate);
-  const stopListening = () => SpeechRecognition.stopListening().then(componentDidUpdate);
-
-  if (error) return <div>{'Request Failed'}</div>;
-	if (isLoading) return <div>{'Loading...'}</div>;
+  if (error) return <div>{"Request Failed"}</div>;
+  if (isLoading) return <div>{"Loading..."}</div>;
   if (!browserSupportsSpeechRecognition) {
-    return <span>{'Browser doesnt support speech recognition.'}</span>;
+    return <span>{"Browser doesnt support speech recognition."}</span>;
   }
 
   return (
@@ -180,28 +190,32 @@ export default function Comments(props: any) {
       <IconBtn onClick={props.logOut}>
         <ArrowLeftIcon className="w-6 h-6"></ArrowLeftIcon>
       </IconBtn>
-      <h2 className="text-3xl/[32px] font-semibold">{'Do you like appples?'}</h2>
+      <h2 className="text-3xl/[32px] font-semibold">
+        {"Do you like appples?"}
+      </h2>
       <h3>{hearts()}</h3>
       <div>
         <ThemeProvider theme={theme}>
-          <ButtonC onClick={deleteComment}>{'-'}</ButtonC>
+          <ButtonC onClick={deleteComment}>{"-"}</ButtonC>
         </ThemeProvider>
-        <ButtonC onClick={submitComment}>{'+'}</ButtonC>
+        <ButtonC onClick={submitComment}>{"+"}</ButtonC>
       </div>
       <div>
-        <h4 className="text-2xl font-medium">{'Leave a Comment:'}</h4>
+        <h4 className="text-2xl font-medium">{"Leave a Comment:"}</h4>
         <TextArea
           placeholder="Write here..."
           onChange={handleChange}
-          value={Comment.content===""?transcript:Comment.content}
-        /> 
-        <p className="text-xl">{'Microphone:'} {listening ? 'on' : 'off'}</p>
-        <ButtonC onClick={startListening}>{'Start Recording'}</ButtonC>
-        <ButtonC onClick={stopListening}>{'Stop Recording'}</ButtonC>
+          value={Comment.content === "" ? transcript : Comment.content}
+        />
+        <p className="text-xl">
+          {"Microphone:"} {listening ? "on" : "off"}
+        </p>
+        <ButtonC onClick={startListening}>{"Start Recording"}</ButtonC>
+        <ButtonC onClick={stopListening}>{"Stop Recording"}</ButtonC>
       </div>
       <div className={styles.commentsArea}>
         <h3 className="text-2xl font-semibold">
-          {'Total Comments'}({Comments.length}):
+          {"Total Comments"}({Comments.length}):
         </h3>
         {Comments.map((CommentItem, index) => {
           return (
