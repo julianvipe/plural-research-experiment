f080fb44d18f57d085d97ec1caa2e01e1666d273
julianvipe <mateo-pat99@hotmail.com>
2024-03-01 14:57:27 -0500

    playfair bug fixxed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import { QueryClient, QueryClientProvider,useQuery } from 'react-query'
 
 const queryClient = new QueryClient()
 
-export const playfair = Playfair(
+const playfair = Playfair(
 {
   subsets:['latin']
 });
